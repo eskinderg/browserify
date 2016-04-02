ce430c36e588cb903baed612de1832fcda991719
Eskinder <eskinderg@users.noreply.github.com>
2016-04-02 19:53:46 -0400

    Watchify Implemented

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 alert("sample alert");
+
+var $ = require('jquery');
+
+var button = $('<button/>').html('Click Me').on('click',function(){
+	alert('clicked');
+	});
+$('body').append(button);
+
+
+
+
+
